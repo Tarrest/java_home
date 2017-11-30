5c3ff5a55a488435a6fef162b220c4349abc9e0a
Tarrest <reva.htp@gmail.com>
2017-11-30 20:07:32 +0200

    Выполнена загрузка тестовых данных из файла для создания группы

--- addressbook/src/test/java/com/chisw/work/addressbook/test/TestGroupCreation.java
+++ addressbook/src/test/java/com/chisw/work/addressbook/test/TestGroupCreation.java
@@ -5,6 +5,7 @@ import com.chisw.work.addressbook.Data.Groups;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,11 +16,15 @@ import static org.hamcrest.MatcherAssert.*;
 public class TestGroupCreation extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> valigGroups() {
+    public Iterator<Object[]> valigGroups() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] {new GroupData().withGroupName("test 1").withGroupComment("header 1").withGroupLogo("footer 1")});
-        list.add(new Object[] {new GroupData().withGroupName("test 2").withGroupComment("header 2").withGroupLogo("footer 2")});
-        list.add(new Object[] {new GroupData().withGroupName("test 3").withGroupComment("header 3").withGroupLogo("footer 3")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new GroupData().withGroupName(split[0]).withGroupComment(split[1]).withGroupLogo(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
