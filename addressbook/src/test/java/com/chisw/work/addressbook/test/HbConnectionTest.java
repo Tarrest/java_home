67204a16b25f9a467c215c84fbebd56fcdbeca28
Tarrest <reva.htp@gmail.com>
2017-12-06 15:21:35 +0200

    connector to DB was added

---
+++ addressbook/src/test/java/com/chisw/work/addressbook/test/HbConnectionTest.java
@@ -0,0 +1,34 @@
+package com.chisw.work.addressbook.test;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HbConnectionTest {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeClass
+    protected void setUp() throws Exception {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+    }
+
+    @Test
+    public void checkHbConnectionTest() {
+
+    }
+}
